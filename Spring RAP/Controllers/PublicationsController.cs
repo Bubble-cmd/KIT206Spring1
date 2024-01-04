5724af36074a5eb497b773be4968a7a12402081d
Bubble-cmd <bubbleteaislove99@gmail.com>
2024-01-04 16:00:16 +1100

    initial folder setup and layout

---
+++ Spring RAP/Controllers/PublicationsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KIT206Spring.Database;
+using KIT206Spring.Entities;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+public class Class1
+{
+	public Class1()
+	{
+		console.log("hi");
+        console.log("hi");
+        console.log("hi");
+    }
+}
