bfab6c1413d72553f97a43f20a0f8c355628698d
Bubble-cmd <bubbleteaislove99@gmail.com>
2024-01-05 19:51:20 +1100

    added PublicationView

---
+++ Spring RAP/View/PublicationView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KIT206Spring.Spring_RAP.Entities
+
+namespace KIT206Spring.Spring_RAP.View
+{
+    internal class PublicationView
+    {
+        public static void PrintAllPublication(Researcher Res)
+        {
+            Console.WriteLine("---\t---\t---\t We are in the Publicaion View \t---\t---\t---");
+            foreach (Publication publication in Res.Pubs)
+            {
+                Console.WriteLine("Publicaiton Name: " + publication.Title);
+            }
+            Console.WriteLine("pause");
+        }
+    }
+}
